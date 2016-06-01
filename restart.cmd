076aaee07528069774969b3cda501bc15a1ee975
Aravindh <aravindh@browserstack.com>
2016-05-31 18:20:17 -0700

    Batch process monitor script

---
+++ restart.cmd
@@ -0,0 +1,31 @@
+@ECHO OFF
+:: Name: restartprocess.cmd
+:: Purpose: Restarts a process if not running
+
+SETLOCAL ENABLEEXTENSIONS
+SET procname=%~1
+IF "%procname%" == "" GOTO :END
+
+FOR /F "tokens=*" %%f in ('dir "C:\" /b /s ^| find "%procname%"') do (
+  SET pname=%%f
+)
+
+:REPEAT
+
+TASKLIST | find "%procname%" > NUL
+IF %ERRORLEVEL% NEQ 0 (
+  IF "%pname%" == "" (
+    GOTO :END
+  )
+  echo "Restarting process..%pame%"
+  START "" "%pname%"
+)
+
+timeout /t 1 /nobreak > NUL
+
+GOTO :REPEAT
+ENDLOCAL
+
+:END
+echo "Process Executable not found !"
+EXIT /B 1
