2b87dbc1c696733669b04daf1d7835b9747bb3a9
Aravindh <aravindh@browserstack.com>
2016-06-06 02:48:32 -0700

    changes to make script work with start-stop-server

--- restart.cmd
+++ restart.cmd
@@ -3,29 +3,34 @@
 :: Purpose: Restarts a process if not running
 
 SETLOCAL ENABLEEXTENSIONS
-SET procname=%~1
-IF "%procname%" == "" GOTO :END
+SET searchst=%~1
+IF "%searchst%" == "" GOTO :END
 
-FOR /F "tokens=*" %%f in ('dir "C:\" /b /s ^| find "%procname%"') do (
-  SET pname=%%f
-)
+SET workingdir=%~2
+IF "%workingdir%" == "" GOTO :END
 
+SET pname=""
 :REPEAT
 
-TASKLIST | find "%procname%" > NUL
-IF %ERRORLEVEL% NEQ 0 (
-  IF "%pname%" == "" (
-    GOTO :END
-  )
-  echo "Restarting process.."
-  START "" "%pname%"
+SET appname=""
+
+FOR /F "tokens=2 delims=," %%i in ('wmic process get CommandLine /format:csv ^| find "%searchst%" ^| find /V "find"') do ( set appname=%%i )
+
+IF NOT "%appname%" == """" (
+    SET pname=%appname%
 )
 
+IF NOT "%pname%" == """" (
+  IF "%appname%" == """" (
+  cd %workingdir% 
+  START %pname% 
+ )
+)
 timeout /t 1 /nobreak > NUL
 
 GOTO :REPEAT
 ENDLOCAL
 
 :END
-echo "Process Executable not found !"
+echo "Invalid input parameters !"
 EXIT /B 1
